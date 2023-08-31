7c97b338e56604c8fbcb5afdf89d4d6d3d1c0b91
Saarang <88927866+saaranganand@users.noreply.github.com>
2023-08-31 19:02:09 +0530

    Update main.cpp
    cleaned up menu a bit

--- src/main.cpp
+++ src/main.cpp
@@ -102,26 +102,40 @@ void StartGame()
 void MainMenu()
 {
     // Draw the game logo
+    /*
+    ! PLACEHOLDER ! 
+    WIP:
+    Change to actual image of game logo once design finished.
+    */
     DrawText("Breakout: Boom", GetScreenWidth() / 2 - MeasureText("Breakout: Boom", 40) / 2, GetScreenHeight() / 4 - 40, 40, WHITE);
 
     // Draw the play button
     DrawRectangle(GetScreenWidth() / 2 - 100, GetScreenHeight() / 2 - 50, 200, 50, BLUE);
     DrawText("Play", GetScreenWidth() / 2 - MeasureText("Play", 30) / 2, GetScreenHeight() / 2 - 40, 30, WHITE);
+    Rectangle button1 = { float(GetScreenWidth() / 2 - 100), float(GetScreenHeight() / 2 - 50), 200, 50 };
 
     // Draw the settings button
     DrawRectangle(GetScreenWidth() / 2 - 100, GetScreenHeight() / 2 + 10, 200, 50, BLUE);
     DrawText("Settings", GetScreenWidth() / 2 - MeasureText("Settings", 30) / 2, GetScreenHeight() / 2 + 20, 30, WHITE);
-
+    Rectangle button2 = { float(GetScreenWidth() / 2 - 100), float(GetScreenHeight() / 2 + 10), 200, 50 };
+    
     // Draw the local leaderboard button
-    DrawRectangle(GetScreenWidth() / 2 - 100, GetScreenHeight() / 2 + 70, 200, 50, BLUE);
+    DrawRectangle(GetScreenWidth() / 2 - 150, GetScreenHeight() / 2 + 70, 300, 50, BLUE);
     DrawText("Local Leaderboard", GetScreenWidth() / 2 - MeasureText("Local Leaderboard", 30) / 2, GetScreenHeight() / 2 + 80, 30, WHITE);
-
+    Rectangle button3 = { float(GetScreenWidth() / 2 - 150), float(GetScreenHeight() / 2 + 70), 300, 50 };
+    
     // Draw other buttons or links as needed
+
+    Vector2 mousePos = GetMousePosition();
+
+    if (CheckCollisionPointRec(mousePos, button1) || CheckCollisionPointRec(mousePos, button2) || CheckCollisionPointRec(mousePos, button3))
+        SetMouseCursor(MOUSE_CURSOR_POINTING_HAND);
+    else
+        SetMouseCursor(MOUSE_CURSOR_DEFAULT);
 
     // Check for user input
     if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
     {
-        Vector2 mousePos = GetMousePosition();
 
         // Check if the play button was clicked
         if (CheckCollisionPointRec(mousePos, {(float)GetScreenWidth() / 2 - 100, (float)GetScreenHeight() / 2 - 50, 200, 50}))
